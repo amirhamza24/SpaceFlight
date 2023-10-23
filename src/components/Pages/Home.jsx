e818c9b6ecc570967cb6bd87ed1af85ca7a505fc
amirhamza24 <ridoykhan7897890@gmail.com>
2023-10-23 15:52:11 +0600

    use context add

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,39 @@
+// import React from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+
+const Home = () => {
+
+    const allData = createContext();
+
+    const [rocketData, setRocketData] = useState([]);
+
+    useEffect( () => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setRocketData())
+    }, [])
+
+
+    return (
+        <div className="w-10/12 mx-auto my-16">
+            <div className="text-center mb-16">
+                <h2 className="text-4xl font-semibold">Spaceflight Details</h2>
+
+                <p className="text-gray-600 leading-10">Find out the elaborate features of all the past big spaceflights.</p>
+            </div>
+
+            <div>
+                <div className='flex items-center relative'>
+                    <input type="text" placeholder="Search..." className='w-4/12 px-3 py-1 focus:outline-none border rounded' />
+
+                    <div className='absolute top-1 left-[335px] rounded-r bg-blue-600 w-10 h-7 flex justify-center items-center text-white'>
+                        <FaSearch></FaSearch>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
