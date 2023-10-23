2043680ab11573019d2701712f940a12fc578e67
amirhamza24 <ridoykhan7897890@gmail.com>
2023-10-24 00:28:31 +0600

    complete

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,5 +1,4 @@
 import { useContext, useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
 import { AllData } from './Context';
 import ShowData from './ShowData';
 
@@ -7,65 +6,73 @@ const Home = () => {
     const { filterDataBySearch, filterDataByStatus, filterDataByDate, currentPage, paginate, totalPages, selectedStatus, selectedDate } = useContext(AllData);
     const [search, setSearch] = useState('');
 
+    // for searching method
     const searchText = (value) => {
         setSearch(value);
         filterDataBySearch(value);
     };
 
+    // filter by Failure, Success
     const handleStatusFilter = (status) => {
         filterDataByStatus(status);
     };
 
+    // filter by date
     const handleDateFilter = (date) => {
         filterDataByDate(date);
     };
 
     return (
-        <div className="w-10/12 mx-auto my-16">
-            <div className="text-center mb-16">
+        <div className="w-11/12 md:w-10/12 mx-auto my-16">
+            <div className="text-left md:text-center mb-16">
                 <h2 className="text-4xl font-semibold">Spaceflight Details</h2>
                 <p className="text-gray-600 leading-10">Find out the elaborate features of all the past big spaceflights.</p>
             </div>
-            <div>
-                <div className="flex items-center relative">
+            
+            <div className='flex flex-col md:flex-row gap-2 justify-between items-center'>
+                <div className="flex items-center relative w-full md:w-3/12">
                     <input
                         type="text"
                         value={search}
                         onChange={(e) => searchText(e.target.value)}
                         placeholder="Search..."
-                        className="w-3/12 px-3 py-1 focus:outline-none border rounded-l"
+                        className="w-full px-3 py-1 focus:outline-none border rounded-l"
                     />
-                    <div className="absolute top-0 left-[280px] rounded-r bg-blue-600 w-10 h-[33px] flex justify-center items-center text-white">
-                        <FaSearch />
+                </div>
+
+                <div className='flex flex-col md:flex-row gap:7 md:gap-4 lg:gap-7'>
+                    <div className="mt-4 flex items-center space-x-4 md:space-x-2 lg:space-x-4">
+                        <label className="text-gray-600">By Launch Status:</label>
+                            <select
+                                value={selectedStatus}
+                                onChange={(e) => handleStatusFilter(e.target.value)}
+                                className="w-36 py-1 bg-white text-gray-700  focus:outline-none border rounded"
+                            >
+                                <option value="All">All</option>
+                                <option value="Success">Success</option>
+                                <option value="Failure">Failure</option>
+                            </select>
+                    </div>
+
+                    <div className="mt-4 flex items-center space-x-4">
+                        <label className="text-gray-600">By Launch Date:</label>
+                            <select
+                                value={selectedDate}
+                                onChange={(e) => handleDateFilter(e.target.value)}
+                                className="w-36 py-1 bg-white text-gray-700 focus:outline-none border rounded"
+                            >
+                                <option value="All">All</option>
+                                <option value="LastWeek">Last Week</option>
+                                <option value="LastMonth">Last Month</option>
+                                <option value="LastYear">Last Year</option>
+                            </select>
                     </div>
                 </div>
             </div>
-            <div className="mt-4 flex items-center space-x-4">
-                <label className="text-gray-600">Filter by Launch Status:</label>
-                <select
-                    value={selectedStatus}
-                    onChange={(e) => handleStatusFilter(e.target.value)}
-                    className="px-4 py-2 bg-gray-200 text-gray-700 rounded"
-                >
-                    <option value="All">All</option>
-                    <option value="Success">Success</option>
-                    <option value="Failure">Failure</option>
-                </select>
-            </div>
-            <div className="mt-4 flex items-center space-x-4">
-                <label className="text-gray-600">Filter by Launch Date:</label>
-                <select
-                    value={selectedDate}
-                    onChange={(e) => handleDateFilter(e.target.value)}
-                    className="px-4 py-2 bg-gray-200 text-gray-700 rounded"
-                >
-                    <option value="All">All</option>
-                    <option value="LastWeek">Last Week</option>
-                    <option value="LastMonth">Last Month</option>
-                    <option value="LastYear">Last Year</option>
-                </select>
-            </div>
+
             <ShowData />
+
+            {/* pagination */}
             {totalPages > 1 && (
                 <div className="flex justify-center mt-4">
                     {Array.from({ length: totalPages }, (_, i) => (
@@ -73,9 +80,9 @@ const Home = () => {
                             key={i + 1}
                             className={`px-4 py-2 ${
                                 currentPage === i + 1 ? 'bg-blue-600 text-white' : 'bg-gray-200 text-gray-700'
-                            }`}
-                            onClick={() => paginate(i + 1)}
-                        >
+                                }`}
+                                onClick={() => paginate(i + 1)}
+                            >
                             {i + 1}
                         </button>
                     ))}
--- src/components/Pages/ShowData.jsx
+++ src/components/Pages/ShowData.jsx
@@ -2,20 +2,22 @@ import { useContext } from 'react';
 import { AllData } from './Context';
 
 const ShowData = () => {
-    const { rocketData, selectedStatus } = useContext(AllData);
+    const { rocketData } = useContext(AllData);
 
     return (
-        <div className="grid grid-cols-3 gap-8 mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
             {rocketData.length > 0 ? (
                 rocketData.map((rocket) => (
                     <div key={rocket.flight_number} className="border flex items-center justify-center p-10">
                         <div className="text-center space-y-3">
                             <img className="w-32 h-32" src={rocket.links.mission_patch_small} alt="" />
                             <p className="text-gray-700">Launch Year: {rocket.launch_year}</p>
+                            
                             <div>
                                 <h2 className="font-semibold text-2xl">{rocket.mission_name}</h2>
                                 <p className="text-gray-700">{rocket.rocket.rocket_name}</p>
                             </div>
+
                             <div>
                                 <p className="text-gray-700">Launch Status:</p>
                                 {rocket.launch_success === true ? (
